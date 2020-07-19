c0133d157f44cf68ad9fa83f3b6a3bfc2f1ca790
Juniorz888 <jclandestine101@gmail.com>
2020-07-19 17:21:54 +0800

    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used in getting the inverse of a matrix 
+## This will be used in long, repeating process to make the computations fast
 
-## Write a short comment describing this function
+## This function returns a list of functions and caches the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    cached_inverse <- NULL 
+    set <- function(new_mat) {                                  ## assigns a new matrix in the parent environment
+        x <<- new_mat
+        inverse <<- NULL
+    }
+    get <- function() x                                         ## gets the matrix
+    setinverse <- function(inverse) cached_inverse <<- inverse  ## caches the computed inverse 
+    getinverse <- function() cached_inverse                     ## gets the cached inverse
+    list(set = set, get = get, 
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+## This function returns the inverse of a matrix. 
+##It checks first if the inverse has been already computed and returns it. 
+##If the inverse is not yet computed, the solve() function and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+                                                ## Returns a matrix that is the inverse of 'x'
+        cached_inverse <- x$getinverse()        ## retrieves the cached inverse
+        if(!is.null(cached_inverse)) {          ## checks if the cached inverse is not null and returns the value
+            message("getting cached data")
+            return(cached_inverse)
+        }
+        data <- x$get()                         ## gets the new matrix to be inversed
+        inverse <- solve(data,...)              ## inverses the matrix
+        x$setinverse(inverse)                   ## setting the computed inverse and caches it
+        inverse 
 }
+
